06ce103329464b97e112a5354d178d6eab8fabae
Isrofil <wertprogram@mail.ru>
2021-11-19 07:01:14 +0500

    19 morning

---
+++ frontend/models/Report.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace frontend\models;
+use yii\base\Model;
+
+class Report extends model
+{
+    public static function MapJoin(){
+        $rows = (new Query())
+        <select name="id" 
+    }
+}
+?>
