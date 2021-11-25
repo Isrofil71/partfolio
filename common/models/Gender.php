f9096560798e204cdd584a76f29d4bf8935faf4f
Isrofil <wertprogram@mail.ru>
2021-11-25 23:01:35 +0500

    26  morning

---
+++ common/models/Gender.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "gender".
+ *
+ * @property int $id
+ * @property string|null $name_uz
+ * @property string|null $name_ru
+ * @property string|null $name_en
+ * @property string|null $name_oz
+ */
+class Gender extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'gender';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name_uz', 'name_ru', 'name_en', 'name_oz'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name_uz' => Yii::t('app', 'Name Uz'),
+            'name_ru' => Yii::t('app', 'Name Ru'),
+            'name_en' => Yii::t('app', 'Name En'),
+            'name_oz' => Yii::t('app', 'Name Oz'),
+        ];
+    }
+    
+    public static function selectList() {
+        $name = 'name_' . Yii::$app->language;
+        return ArrayHelper::map(self::find()->all(), 'id', $name);
+    }
+}
