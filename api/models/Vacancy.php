5ec2bd9dc7ba55586c875ba6a5608fbc9b3cac39
Isrofil <wertprogram@mail.ru>
2021-12-03 21:19:54 +0500

    add

--- api/models/Vacancy.php
+++ api/models/Vacancy.php
@@ -1,9 +1,27 @@
 <?php
 namespace api\models;
 
-class Vacancy extends common\models\Vacancy {
+class Vacancy extends \common\models\Vacancy {
     public function fields(){
-        return parent::fields();
+        return [
+            'id',
+            'profession' => function($model){
+                return $model->profession ? $model->profession->name_uz : " ";
+            },
+            'image' => function($model){
+                return "http://isrofil.smartdesign.uz/frontend/web/img/vacancy/" . $model->image;
+            },
+            'company' =>function($model){
+                return $model->company ? $model->company->name : " ";
+            },
+            'address',
+            'jobtype' =>function($model){
+                return $model->jobType ? $model->jobType->name_uz : " ";
+            },
+            'link' =>function($model){
+                return 'http://isrofil.smartdesign.uz/vacancy/single?id=' . $model->id;
+            },
+        ];
     }
 }
 ?>
